39c247cdcfe0db000f3d0938add60f0c938bbc97
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-02-04 13:35:14 +0100

    feat: add routes and create Home and Movies pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BaseLayout from "./components/layouts/BaseLayout";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<BaseLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
