16f0788679ebc0f9ba7868a539e4e53e1786695c
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-02-04 14:03:34 +0100

    feat: add test API call

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,3 +1,16 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/movies")
+      .then((res) => setMovies(res.data.data));
+  }, []);
+
+  console.log(movies);
+
   return <h1>Movies</h1>;
 }
