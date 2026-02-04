5aa64eb86269003fc8a1c0c2b7ccfb8d1f3be30d
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-02-04 17:24:16 +0100

    feat(movieDetails): add API call

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function MovieDetailsPage() {
   const { id } = useParams();
 
-  return <h1>Dettaglio film {id}</h1>;
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/movies/${id}`)
+      .then((res) => setMovie(res.data))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  console.log(movie);
+
+  return (
+    <section>
+      {movie && (
+        <h1>
+          {movie.title}
+          {id}
+        </h1>
+      )}
+    </section>
+  );
 }
