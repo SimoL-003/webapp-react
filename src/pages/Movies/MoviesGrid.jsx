e1a1a7f404b3b5ccd2159c1d2aae93b177096155
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-02-04 16:37:39 +0100

    feat(moviesPage): add skeleton loader

--- src/pages/Movies/MoviesGrid.jsx
+++ src/pages/Movies/MoviesGrid.jsx
@@ -1,27 +1,28 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
+import MovieCardSkeleton from "./MovieCardSkeleton";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get("http://localhost:3000/api/movies")
-      .then((res) => setMovies(res.data.data));
+      .then((res) => setMovies(res.data.data))
+      .finally(() => setLoading(false));
   }, []);
 
   console.log(movies);
 
   return (
     <>
-      {movies && (
-        <section className="bg-white grid grid-cols-3 gap-4 items-center rounded-lg p-4">
-          {movies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
-          ))}
-        </section>
-      )}
+      <section className="bg-white grid grid-cols-3 gap-4 items-center rounded-lg p-4">
+        {loading
+          ? [1, 2, 3].map((num) => <MovieCardSkeleton key={num} />)
+          : movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </section>
     </>
   );
 }
