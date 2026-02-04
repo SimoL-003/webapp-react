e6e10519a9bf157bc774e32b315d890cd4180ce2
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-02-04 14:52:20 +0100

    feat(header): add header

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,3 +1,38 @@
+import { Link, NavLink } from "react-router-dom";
+
 export default function Header() {
-  return <h1>Header</h1>;
+  const navLinks = [
+    { pageTitle: "Home", path: "/" },
+    { pageTitle: "Movies", path: "/movies" },
+  ];
+
+  return (
+    <header className="h-20 bg-slate-900 border-b border-s-slate-700">
+      <div className="container flex justify-between items-center h-full">
+        <div>
+          <Link
+            to="/"
+            className="flex items-center gap-2 text-slate-100 font-semibold text-lg"
+          >
+            <span className="text-indigo-400 text-xl">ICONA</span>MovieCatalogue
+          </Link>
+        </div>
+
+        <nav className="text-slate-300">
+          <ul className="flex gap-6">
+            {navLinks.map((page) => (
+              <li
+                key={page.path}
+                className="hover:text-indigo-300 transition-colors"
+              >
+                <NavLink to={page.path} className="px-4 py-2 transition-all">
+                  {page.pageTitle}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
