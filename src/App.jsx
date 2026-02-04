322411343dd5856822df8f32119e5b11d3500464
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-02-04 16:48:02 +0100

    feat(notFound): add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BaseLayout from "./components/layouts/BaseLayout";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route element={<BaseLayout />}>
             <Route index element={<Home />} />
             <Route path="/movies" element={<Movies />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
